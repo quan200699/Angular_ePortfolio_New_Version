cfaf472f98231ec41779ef3059491b99541bf8cd
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-14 22:22:55 +0700

    add layout with shared done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {LayoutWithSharedComponent} from './layout/layout-with-shared/layout-with-shared.component';
 
 
 const routes: Routes = [
   {
-    path: 'portfolio',
-    loadChildren: () => import('./portfolio/portfolio.module').then(module => module.PortfolioModule)
+    path: '',
+    component: LayoutWithSharedComponent,
+    children: [
+      {
+        path: 'portfolio',
+        loadChildren: () => import('./portfolio/portfolio.module').then(module => module.PortfolioModule)
+      }
+    ]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
+import { LayoutWithSharedComponent } from './layout/layout-with-shared/layout-with-shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutWithSharedComponent
   ],
   imports: [
     BrowserModule,
