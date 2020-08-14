6e8665ebbeba67111144f2657d7057241580a41f
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-14 10:28:46 +0700

    add portfolio component and moudle

---
+++ src/app/portfolio/portfolio.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {PortfolioComponent} from './portfolio.component';
+import {PortfolioRoutingModule} from './portfolio-routing.module';
+
+
+@NgModule({
+  declarations: [
+    PortfolioComponent
+  ],
+  imports: [
+    PortfolioRoutingModule
+  ]
+})
+export class PortfolioModule {
+}
