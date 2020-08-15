09d510e588ebf01d0cf344b307999e581ce05845
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-15 10:12:14 +0700

    add routing to login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LayoutWithSharedComponent} from './layout/layout-with-shared/layout-with-shared.component';
+import {LoginComponent} from './login/login.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,10 @@ const routes: Routes = [
         loadChildren: () => import('./portfolio/portfolio.module').then(module => module.PortfolioModule)
       }
     ]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
