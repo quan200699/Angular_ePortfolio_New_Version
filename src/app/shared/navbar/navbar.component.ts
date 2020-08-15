8b9618f9b44e04238ae32f34d873271992c16c99
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-15 10:38:07 +0700

    add navbar login and logout function

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {UserToken} from '../../interface/user-token';
+import {User} from '../../interface/user';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../service/authentication/authentication.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  currentUser: UserToken;
+  hasRoleAdmin = false;
+  hasRoleUser = false;
+  user: User;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService) {
+    this.authenticationService.currentUser.subscribe(value => this.currentUser = value);
+    if (this.currentUser) {
+      const roleList = this.currentUser.roles;
+      for (const role of roleList) {
+        if (role.authority === 'ADMIN') {
+          this.hasRoleAdmin = true;
+        } else {
+          this.hasRoleUser = true;
+        }
+      }
+    }
   }
 
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
