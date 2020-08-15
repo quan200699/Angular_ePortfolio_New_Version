afe05d394dacd4eef2783eae4f519149262d0e39
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-15 10:11:11 +0700

    add helper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
 import { LayoutWithSharedComponent } from './layout/layout-with-shared/layout-with-shared.component';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {JwtInterceptor} from './helper/jwt-interceptor';
+import {ErrorInterceptor} from './helper/error-interceptor';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { LayoutWithSharedComponent } from './layout/layout-with-shared/layout-wi
     AppRoutingModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
