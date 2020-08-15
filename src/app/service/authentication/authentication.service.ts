062a6ec2fd00be39356bdc6c68594ac12bb9c9ec
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-15 10:09:37 +0700

    add authentication service

---
+++ src/app/service/authentication/authentication.service.ts
@@ -0,0 +1,41 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {map} from 'rxjs/operators';
+import {UserToken} from '../../interface/user-token';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+
+const API_URL = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<UserToken>;
+  public currentUser: Observable<UserToken>;
+  update = new EventEmitter<string>();
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<UserToken>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): UserToken {
+    return this.currentUserSubject.value;
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<any>(API_URL + '/login', {email, password})
+      .pipe(map(user => {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        this.currentUserSubject.next(user);
+        this.update.emit('login');
+        return user;
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
