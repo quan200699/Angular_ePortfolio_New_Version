00a38c6ba65f88584a96b6d1abc60acf63800c98
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-15 10:27:37 +0700

    login component is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
 import { LayoutWithSharedComponent } from './layout/layout-with-shared/layout-with-shared.component';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {JwtInterceptor} from './helper/jwt-interceptor';
 import {ErrorInterceptor} from './helper/error-interceptor';
 import { LoginComponent } from './login/login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,9 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthenticationService} from '../service/authentication/authentication.service';
+import {first} from 'rxjs/operators';
+import {User} from '../interface/user';
+
+declare var $: any;
+declare var Swal: any;
 
 @Component({
   selector: 'app-login',
@@ -6,10 +14,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginRequest: User = {
+    email: '',
+    password: ''
+  }
+  returnUrl: string;
+  loading = false;
+  submitted = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private authenticationService: AuthenticationService) {
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
   }
 
+  ngOnInit() {
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl || '/';
+  }
+
+  login() {
+    this.submitted = true;
+    this.loading = true;
+    this.authenticationService.login(this.loginRequest.email, this.loginRequest.password)
+      .pipe(first())
+      .subscribe(
+        data => {
+          localStorage.setItem('ACCESS_TOKEN', data.accessToken);
+          this.router.navigate([this.returnUrl]).finally(() => {
+          });
+          $(function() {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'top-end',
+              showConfirmButton: false,
+              timer: 3000
+            });
+
+            Toast.fire({
+              type: 'success',
+              title: 'Đăng nhập thành công'
+            });
+          });
+        },
+        () => {
+          this.loading = false;
+          $(function() {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'top-end',
+              showConfirmButton: false,
+              timer: 3000
+            });
+
+            Toast.fire({
+              type: 'error',
+              title: 'Đăng nhập thất bại'
+            });
+          });
+        });
+  }
 }
