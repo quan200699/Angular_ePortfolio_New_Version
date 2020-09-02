9b3c55ff7cad98184b4beb1f87e741718001094d
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-02 10:31:57 +0700

    add lecture module

---
+++ src/app/lecture/lecture.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {LectureRoutingModule} from './lecture-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LectureRoutingModule
+  ]
+})
+export class LectureModule { }
