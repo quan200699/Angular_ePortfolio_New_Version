cf3365d2c8118f4d02dc3619a8e4f55f482d1333
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-02 15:22:44 +0700

    create lecture done

--- src/app/lecture/lecture-create/lecture-create.component.ts
+++ src/app/lecture/lecture-create/lecture-create.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Lecture} from '../../interface/lecture';
 import {LectureService} from '../../service/lecture/lecture.service';
+import {AuthenticationService} from '../../service/authentication/authentication.service';
+import {User} from '../../interface/user';
+import {NotificationService} from '../../service/notification/notification.service';
+import {UserService} from '../../service/user/user.service';
 
 @Component({
   selector: 'app-lecture-create',
@@ -14,13 +18,44 @@ export class LectureCreateComponent implements OnInit {
     lectureId: ''
   };
 
-  constructor(private lectureService: LectureService) {
+  constructor(private lectureService: LectureService,
+              private authenticationService: AuthenticationService,
+              private notificationService: NotificationService,
+              private userService: UserService) {
   }
 
   ngOnInit() {
   }
 
-  createLecture() {
-
+  async createLecture() {
+    if (this.lecture.email != '') {
+      let user = await this.register(this.lecture.email);
+      if (user != null) {
+        user = await this.getUserByEmail(user.email);
+        if (user != null) {
+          this.lecture.user = user;
+          this.lectureService.createLecture(this.lecture).subscribe(() => {
+            this.notificationService.showSuccessMessage('Tạo thành công!');
+          }, () => {
+            this.notificationService.showErrorMessage('Xảy ra lỗi!');
+          });
+        }
+      }
+    } else {
+      this.notificationService.showErrorMessage('Chưa điền email!');
+    }
   }
+
+  register(email) {
+    let user: User = {
+      email: email,
+      password: '123456@Abc'
+    };
+    return this.authenticationService.register(user).toPromise();
+  }
+
+  getUserByEmail(email) {
+    return this.userService.getUserByEmail(email).toPromise();
+  }
+
 }
