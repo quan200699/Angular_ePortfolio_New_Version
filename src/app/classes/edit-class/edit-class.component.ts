18e031d2875659606208a6605423984a8e6540fc
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-03 12:59:30 +0700

    edit class has select option for lecture

--- src/app/classes/edit-class/edit-class.component.ts
+++ src/app/classes/edit-class/edit-class.component.ts
@@ -3,6 +3,8 @@ import {Classes} from '../../interface/classes';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 import {ClassesService} from '../../service/classes/classes.service';
 import {NotificationService} from '../../service/notification/notification.service';
+import {LectureService} from '../../service/lecture/lecture.service';
+import {Lecture} from '../../interface/lecture';
 
 @Component({
   selector: 'app-edit-class',
@@ -10,27 +12,27 @@ import {NotificationService} from '../../service/notification/notification.servi
   styleUrls: ['./edit-class.component.css']
 })
 export class EditClassComponent implements OnInit {
-  classes: Classes = {
-    name: '',
-    module: {
-      id: -1
-    },
-    lecture: {
-      id: -1
-    }
-  };
+  classes: Classes = {};
+  listLecture: Lecture[] = [];
   id: number;
 
   constructor(private activatedRoute: ActivatedRoute,
               private notificationService: NotificationService,
-              private classesService: ClassesService) {
+              private classesService: ClassesService,
+              private lectureService: LectureService) {
   }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(async (paramMap: ParamMap) => {
       this.id = +paramMap.get('id');
       this.classes = await this.getClasses(this.id);
+      if (this.classes.lecture == null) {
+        this.classes.lecture = {
+          id: -1
+        };
+      }
     });
+    this.getAllLecture();
   }
 
   getClasses(id: number) {
@@ -39,9 +41,15 @@ export class EditClassComponent implements OnInit {
 
   updateClasses(id: number) {
     this.classesService.updateClasses(id, this.classes).subscribe(() => {
-      this.notificationService.showSuccessMessage("Cập nhật thành công!")
+      this.notificationService.showSuccessMessage('Cập nhật thành công!');
     }, () => {
-      this.notificationService.showErrorMessage("Cập nhật thất bại!")
+      this.notificationService.showErrorMessage('Cập nhật thất bại!');
+    });
+  }
+
+  getAllLecture() {
+    this.lectureService.getAllLecture().subscribe(listLecture => {
+      this.listLecture = listLecture;
     });
   }
 }
