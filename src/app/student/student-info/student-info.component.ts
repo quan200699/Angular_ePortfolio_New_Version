f60cb52f7da5e1d32f03e4a7e2d8487a77f5b2c2
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-03 22:32:56 +0700

    add get student info

--- src/app/student/student-info/student-info.component.ts
+++ src/app/student/student-info/student-info.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {Student} from '../../interface/student';
+import {StudentService} from '../../service/student/student.service';
 
 @Component({
   selector: 'app-student-info',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-info.component.css']
 })
 export class StudentInfoComponent implements OnInit {
+  student: Student = {};
+  id: number;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private studentService: StudentService) {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.id = +paramMap.get('id');
+      this.getStudent(this.id);
+    });
+  }
 
   ngOnInit() {
   }
 
+  getStudent(id: number) {
+    this.studentService.getStudent(id).subscribe(student => {
+      this.student = student;
+    });
+  }
 }
