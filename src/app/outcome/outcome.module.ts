80b45730d1f726936968a113963570edc8226df6
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-04 20:51:33 +0700

    add outcome module

---
+++ src/app/outcome/outcome.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {OutcomeRoutingModule} from './outcome-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OutcomeRoutingModule
+  ]
+})
+export class OutcomeModule { }
