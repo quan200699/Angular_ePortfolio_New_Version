5fd48e049de4e80d6ff673826e6cbaf8f0729bc0
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-04 22:19:23 +0700

    add category list to component

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserToken} from '../../interface/user-token';
+import {NotificationService} from '../../service/notification/notification.service';
+import {AuthenticationService} from '../../service/authentication/authentication.service';
+import {Category} from '../../interface/category';
+import {CategoryService} from '../../service/category/category.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +14,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  listCategory: Category[];
+  currentUser: UserToken;
+  hasRoleAdmin = false;
+  id: number;
 
-  ngOnInit() {
+  constructor(private categoryService: CategoryService,
+              private notificationService: NotificationService,
+              private authenticationService: AuthenticationService) {
+    this.authenticationService.currentUser.subscribe(value => this.currentUser = value);
+    if (this.currentUser) {
+      const roleList = this.currentUser.roles;
+      for (const role of roleList) {
+        if (role.authority === 'ADMIN') {
+          this.hasRoleAdmin = true;
+        }
+      }
+    }
   }
 
+  ngOnInit() {
+    this.getAllLecture();
+  }
+
+  getCategoryId(id: number) {
+    this.id = id;
+  }
+
+  deleteCategory() {
+    this.categoryService.deleteCategory(this.id).subscribe(() => {
+      this.categoryService.getAllCategory().subscribe(listCategory => {
+        this.listCategory = listCategory;
+      });
+      $(function() {
+        $('#modal-delete').modal('hide');
+      });
+      this.notificationService.showSuccessMessage('Xóa thành công!');
+    }, () => {
+      this.notificationService.showErrorMessage('Xóa thất bại!');
+    });
+  }
+
+  getAllLecture() {
+    this.categoryService.getAllCategory().subscribe(listCategory => {
+      this.listCategory = listCategory;
+      $(function() {
+        $('#table-category').DataTable({
+          'paging': true,
+          'lengthChange': false,
+          'searching': false,
+          'ordering': true,
+          'info': true,
+          'autoWidth': false,
+        });
+      });
+    });
+  }
 }
--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CategoryListComponent} from './category-list/category-list.component';
+import {CategoryRoutingModule} from './category-routing.module';
 
 
 
@@ -9,7 +10,8 @@ import {CategoryListComponent} from './category-list/category-list.component';
     CategoryListComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CategoryRoutingModule
   ]
 })
 export class CategoryModule { }
