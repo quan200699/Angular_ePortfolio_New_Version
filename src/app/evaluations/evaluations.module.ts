4518fa8736b584ad0437afa57ebdeb5bf1adb40c
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-05 09:45:57 +0700

    add evaluation module

---
+++ src/app/evaluations/evaluations.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {EvaluationsRoutingModule} from './evaluations-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EvaluationsRoutingModule
+  ]
+})
+export class EvaluationsModule { }
